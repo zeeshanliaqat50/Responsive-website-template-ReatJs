346d933d2baea13b179fb8c22669febe0117d878
zeeshanliaqat50 <zeeshanliaqat90@gmail.com>
2022-05-12 14:48:21 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import About from "./Components/About";
+import ServiceA from "./Components/ServiceA";
+import ServiceB from './Components/ServiceB'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+      <BrowserRouter>
+      <NavBar />
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/sundayservice" element={<ServiceA/>}></Route>
+          <Route path="/upcomingevents" element={<ServiceB/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
